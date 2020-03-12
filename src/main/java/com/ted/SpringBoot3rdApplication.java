61cac20a8ee58cfa0b2834e7e4deb45562b65ca2
Ted <ted0432912394@gmail.com>
2020-03-12 23:14:20 +0800

    spring-boot-3rd

---
+++ src/main/java/com/ted/SpringBoot3rdApplication.java
@@ -0,0 +1,13 @@
+package com.ted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3rdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3rdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ted/SpringBoot3rdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3rdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
